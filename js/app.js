8849c30d2436fd9eaf1c5227ce6b1d65266707f1
Raman Batra <raman.batra1709@gmail.com>
2018-10-28 17:27:15 +0530

    Allow tasks to be filtered

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 addEventListeners();
@@ -9,6 +10,7 @@ function addEventListeners()    {
     form.addEventListener('submit', addTask);
     taskList.addEventListener('click', removeTask);
     clearBtn.addEventListener('click', clearTasks);
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(event) {
@@ -40,4 +42,16 @@ function clearTasks()   {
     while(taskList.firstChild)  {
         taskList.removeChild(taskList.firstChild);
     }
+}
+
+function filterTasks(event)  {
+    const text = event.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task)    {
+        if (task.firstChild.textContent.toLowerCase().indexOf(text) != -1)  {
+            task.style.display = 'block';
+        }
+        else    {
+            task.style.display = 'none';
+        }
+    });
 }
